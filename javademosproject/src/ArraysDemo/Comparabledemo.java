3cb70c5ab1be48cf782ab8dcf7b8c8eec32d7b48
Hithesh G. Raj <hiraj@BLR.ALLEGISINDIA.COM>
2023-03-05 21:56:33 +0530

    Comparatordemo

---
+++ javademosproject/src/ArraysDemo/Comparabledemo.java
@@ -0,0 +1,51 @@
+package ArraysDemo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class Comparabledemo{
+	public static void main(String[] args) throws InterruptedException {
+		List list=new ArrayList();
+		list.add(new Employee(105,"mark",15000,"AA2001"));
+		list.add(new Employee(102,"varsha",12000,"AA4001"));
+		list.add(new Employee(13,"darshan",16000,"AA7001"));
+		Collections.sort(list);
+		for(int i=0;i<list.size();i++) {
+			System.out.println(list.get(i));
+		}
+	}
+}
+//This Comparable method overrides the Collection.sort to required comparison
+class Employee implements Comparable<Employee> {
+	int id;
+	String name;
+	double salary;
+	String aadhar;
+	
+	public Employee(int id, String name, double salary, String aadhar) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.aadhar = aadhar;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", aadhar=" + aadhar + "]";
+	}
+//This is with respect to id
+	/*@Override
+	public int compareTo(Employee o) {
+		// TODO Auto-generated method stub
+		return this.id-(o.id);
+	}*/
+	@Override
+	public int compareTo(Employee o) {
+		// TODO Auto-generated method stub
+		return this.aadhar.compareTo(o.aadhar);
+	}
+	
+}
--- javademosproject/src/ArraysDemo/Comparatordemo.java
+++ javademosproject/src/ArraysDemo/Comparatordemo.java
@@ -2,50 +2,45 @@ package ArraysDemo;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
-public class Comparatordemo{
+public class Comparatordemo {
+
 	public static void main(String[] args) throws InterruptedException {
 		List list=new ArrayList();
-		list.add(new Employee(105,"mark",15000,"AA2001"));
-		list.add(new Employee(102,"varsha",12000,"AA4001"));
-		list.add(new Employee(13,"darshan",16000,"AA7001"));
-		Collections.sort(list);
+		list.add(new Leaner(105,"mark",15000,"AA2001"));
+		list.add(new Leaner(102,"varsha",12000,"AA4001"));
+		list.add(new Leaner(13,"darshan",16000,"AA7001"));
+		Collections.sort(list,new Compare());
 		for(int i=0;i<list.size();i++) {
 			System.out.println(list.get(i));
 		}
 	}
 }
-//This Comparable method overrides the Collection.sort to required comparison
-class Employee implements Comparable<Employee> {
+class Leaner{
 	int id;
 	String name;
 	double salary;
 	String aadhar;
-	
-	public Employee(int id, String name, double salary, String aadhar) {
-		super();
+	public Leaner(int id, String name, double salary, String aadhar) {
 		this.id = id;
 		this.name = name;
 		this.salary = salary;
 		this.aadhar = aadhar;
 	}
-
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", aadhar=" + aadhar + "]";
 	}
-//This is with respect to id
-	/*@Override
-	public int compareTo(Employee o) {
-		// TODO Auto-generated method stub
-		return this.id-(o.id);
-	}*/
-	@Override
-	public int compareTo(Employee o) {
-		// TODO Auto-generated method stub
-		return this.name.compareTo(o.name);
-	}
 	
+}
+class Compare implements Comparator<Leaner>{
+
+	@Override
+	public int compare(Leaner o1, Leaner o2) {
+		// TODO Auto-generated method stub
+		return o1.aadhar.compareTo(o2.aadhar);
+	}
+
 }
